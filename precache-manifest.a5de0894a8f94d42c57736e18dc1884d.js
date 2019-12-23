52ca73d57c66882f3a39b4ab2f5cbc3ba900c75a
pozithron <selcuktoklucu@gmail.com>
2019-12-22 19:01:28 -0500

    Updates

---
+++ precache-manifest.a5de0894a8f94d42c57736e18dc1884d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4f6fb841d77d5af74d0e9c5e1d52125",
+    "url": "/dotdbWebTypeScript/index.html"
+  },
+  {
+    "revision": "1a19fc13b63cc05056f5",
+    "url": "/dotdbWebTypeScript/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "4dd4c9b8ac0ef0d17169",
+    "url": "/dotdbWebTypeScript/static/css/main.fe47ac33.chunk.css"
+  },
+  {
+    "revision": "1a19fc13b63cc05056f5",
+    "url": "/dotdbWebTypeScript/static/js/2.9ba4e87a.chunk.js"
+  },
+  {
+    "revision": "4aa0fe1eaee3dcf45338a38641d40f08",
+    "url": "/dotdbWebTypeScript/static/js/2.9ba4e87a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4dd4c9b8ac0ef0d17169",
+    "url": "/dotdbWebTypeScript/static/js/main.fa68d4e5.chunk.js"
+  },
+  {
+    "revision": "aa757331ffe04d68ceab",
+    "url": "/dotdbWebTypeScript/static/js/runtime-main.ccdfde96.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dotdbWebTypeScript/static/media/logo.5d5d9eef.svg"
+  }
+]);
